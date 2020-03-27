115ef9c6d59d3808241a38dc24ebcd11022b58a6
Ali <a_fervent@outlook.com>
2020-03-27 18:33:00 +0600

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/DetailedActivity.java
@@ -0,0 +1,50 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class DetailedActivity extends AppCompatActivity {
+
+    TextView tvTitle,tvSource,tvTime,tvDesc;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed);
+        tvTitle = findViewById(R.id.tvTitle);
+        tvSource = findViewById(R.id.tvSource);
+        tvTime = findViewById(R.id.tvDate);
+        tvDesc = findViewById(R.id.tvDesc);
+
+        imageView = findViewById(R.id.imageView);
+
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        String source = intent.getStringExtra("source");
+        String time = intent.getStringExtra("time");
+        String desc = intent.getStringExtra("desc");
+        String imageUrl = intent.getStringExtra("imageUrl");
+        String url = intent.getStringExtra("url");
+
+
+        tvTitle.setText(title);
+        tvSource.setText(source);
+        tvTime.setText(time);
+        tvDesc.setText(desc);
+
+        Picasso.with(DetailedActivity.this).load(imageUrl).into(imageView);
+
+        
+    }
+}
